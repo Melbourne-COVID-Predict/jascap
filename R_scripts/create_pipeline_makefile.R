8c1ea5f437d640dd34df13566bc7fa147999a022
eroesti <elisa.roesti@gmail.com>
2022-11-11 14:15:20 +1100

    adding reports commands in create_pipeline_makefile and first report - empty droplets.Rmd

--- R_scripts/create_pipeline_makefile.R
+++ R_scripts/create_pipeline_makefile.R
@@ -69,13 +69,15 @@ output_directory = glue("{result_directory}/preprocessing_results/empty_droplet_
 output_path_empty_droplets =   glue("{output_directory}/{samples}{suffix}_output.rds")
 output_path_plot_pdf =  glue("{output_directory}/{samples}{suffix}_plot.pdf")
 output_path_plot_rds =  glue("{output_directory}/{samples}{suffix}_plot.rds")
+output_path_report = glue("{output_directory}/{samples}{suffix}_empty_droplets_report.md")
 
 # Create input
 commands =
   commands |>
   c(
     glue("CATEGORY={suffix}\nMEMORY=10024\nCORES=1\nWALL_TIME=30000"),
-    glue("{output_path_empty_droplets} {output_path_plot_pdf} {output_path_plot_rds}:{input_path_demultiplexed}\n{tab}Rscript {R_code_directory}/run{suffix}.R {code_directory} {input_path_demultiplexed} {filtered} {output_path_empty_droplets} {output_path_plot_pdf} {output_path_plot_rds}")
+    glue("{output_path_empty_droplets} {output_path_plot_pdf} {output_path_plot_rds}:{input_path_demultiplexed}\n{tab}Rscript {R_code_directory}/run{suffix}.R {code_directory} {input_path_demultiplexed} {filtered} {output_path_empty_droplets} {output_path_plot_pdf} {output_path_plot_rds}"),
+    glue("{output_path_report}:{input_path_demultiplexed} {output_path_empty_droplets}\n{tab}Rscript -e rmarkdown::render('{R_code_directory}/empty_droplets_analysis.Rmd {input_path_demultiplexed} {output_path_empty_droplets}')"
   )
 
 
